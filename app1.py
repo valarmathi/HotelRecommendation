b81269e1f4f0e7ad3cc824876c64c02db68bd312
valarmathi <valarkannan@gmail.com>
2024-06-05 11:25:57 +0530

    Delete app1.py

--- app1.py
+++
@@ -1,9 +0,0 @@
-from HotelRecommendationBot import HotelRecommendationBot
-if __name__=='__main__':
-    print("Welcome to Hotel Recommendation chatbot");
-    print("===================================================");
-    hotelBot = HotelRecommendationBot()
-    #hotelBot.dialog_management_system()
-    hotelBot.deploy()
-    print("===================================================");
-    print("Thank you for choosing Hotel Recommendation chatbot");
